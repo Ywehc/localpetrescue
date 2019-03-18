b1023e9aa775e44c6a003b4d3915224434e8758c
Chris <chris.kieran@gmail.com>
2019-03-18 19:23:15 -0400

    initial commit

---
+++ build/precache-manifest.b1dc3803a9cf275599a0392ca4102346.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "a983b22f529e3da70c7f",
+    "url": "/static/js/main.abe2b72c.chunk.js"
+  },
+  {
+    "revision": "0616f227e3a54ff92bcb",
+    "url": "/static/js/2.081b258a.chunk.js"
+  },
+  {
+    "revision": "a983b22f529e3da70c7f",
+    "url": "/static/css/main.c28aeb20.chunk.css"
+  },
+  {
+    "revision": "0f9056127769f2ee6e28b6d7da75db3e",
+    "url": "/index.html"
+  }
+];
